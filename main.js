2ced1d4083d14b2516aa61480c56aa8ab7c3cac1
Pyry Kovanen <pyry.kovanen@gmail.com>
2016-08-29 16:17:12 -0700

    Add function to convert weight to color

--- main.js
+++ main.js
@@ -9,17 +9,17 @@ function initMap() {
   var bikeLayer = new google.maps.BicyclingLayer();
   bikeLayer.setMap(map);
   var coords = [
-    {lat: 60.165092, lng: 24.930971, color: '#FF0000'},
-    {lat: 60.158126, lng: 24.945420, color: '#00FF00'},
-    {lat: 60.1699, lng: 24.9384, color: '#00FF00'}
+    {lat: 60.165092, lng: 24.930971, weight: 0.8},
+    {lat: 60.158126, lng: 24.945420, weight: 0.5},
+    {lat: 60.1699, lng: 24.9384, weight: 0.1}
   ];
 
   coords.forEach(function(coord) {
     var cityCircle = new google.maps.Circle({
-      strokeColor: coord.color,
+      strokeColor: getColor(coord.weight),
       strokeOpacity: 0.8,
       strokeWeight: 2,
-      fillColor: coord.color,
+      fillColor: getColor(coord.weight),
       fillOpacity: 0.35,
       map: map,
       center: coord,
@@ -28,4 +28,8 @@ function initMap() {
   });
 }
 
-
+function getColor(value){
+  //value from 0 to 1
+  var hue=((1-value)*120).toString(10);
+  return ["hsl(",hue,",100%,50%)"].join("");
+}
