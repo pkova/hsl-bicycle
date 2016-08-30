86e2f721aa722aa33e909109f0e22e6135cc36a8
pautere <pekka.autere@gmail.com>
2016-08-30 20:45:04 +0300

    Now a working version

--- bayesfit.R
+++ bayesfit.R
@@ -1,41 +1,50 @@
 library(MASS)
 
+be = matrix(c(2,3),2,1)
+x=matrix(rexp(200, rate=.1),20,2)
+y=x%*%be+rnorm(20,0,1)
 
-bayesReg <- function(x, y, params = c(burn = 300,
+out <- bayesReg(x,y)
+
+bayesReg <- function(x, y, burn = 300,
 				      rounds = 500,
 				      thin = 10,
 				      nu = 3,
-				      allwt =,
-				      prior =,)) {
+				      allwt,
+				      prior) {
 					
 
-params <- ncol(x)
-samples <- nrow(x)
-#be = matrix(c(2,3),2,1)
-#x=matrix(rexp(200, rate=.1),samples,params)
-#y=x%*%be+rnorm(20,0,1)
+numparams <- ncol(x)
+numsamples <- nrow(x)
 
-numpar <- 1 + params + samples
+numpar <- 1 + numparams + numsamples
 
-if (length(allwt) == 0) {
-	allwt <- matrix(runif(params*1), params,1)
+if (missing(allwt)) {
+	allwt <- matrix(c(1,rnorm(numpar-1,0,1)), numpar,1)
+}
+
+if (missing(prior)) {
+  prior <- diag(1, numparams,numparams)
 }
 
 wt <- allwt[2:(numpar+1)]
 
 for (n in 1:burn){
-	allwt <- bayesSample(allwt,wt,x,y)
+	allwt <- bayesSample(allwt,wt,x,y,nu,prior)
 }
 
-weights <- matrix(0,params,rounds)
+weights <- matrix(0,numparams,rounds)
 hyper <- matrix(0,1,rounds)
 
 for (i in 1:rounds) {
 	for (j in 1:thin){
-		allwt <- bayesSample(allwt,param,x,y)
+		allwt <- bayesSample(allwt,wt,x,y,nu,prior)
 	}
-  	weights[,i] <- allwt[2:(params+1)]
+  	weights[,i] <- allwt[2:(numparams+1)]
   	hyper[,i] <- allwt[1]
+}
+output=list(weights,hyper)
+return(output)
 }
 
 repmat <- function(X,m,n) {
