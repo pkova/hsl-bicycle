970b2c7668e3df19c2359e93e0ea529c6603cb07
Pyry Kovanen <pyry.kovanen@gmail.com>
2016-08-30 14:57:32 -0700

    Refactor rendering to own function

--- client/main.js
+++ client/main.js
@@ -1,4 +1,11 @@
 var map;
+
+var coords = [
+  {lat: 60.165092, lng: 24.930971, total_slots: 20, available_bikes: 1, location_name: 'Kaisaniemi'},
+  {lat: 60.158126, lng: 24.945420, total_slots: 15, available_bikes: 3, location_name: 'Ruoholahti'},
+  {lat: 60.1699, lng: 24.9384, total_slots: 25, available_bikes: 0, location_name: 'Niemenmäki'}
+];
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 60.1699, lng: 24.9384},
@@ -9,12 +16,16 @@ function initMap() {
   var bikeLayer = new google.maps.BicyclingLayer();
   bikeLayer.setMap(map);
 
-  var coords = [
-    {lat: 60.165092, lng: 24.930971, total_slots: 20, available_bikes: 1, location_name: 'Kaisaniemi'},
-    {lat: 60.158126, lng: 24.945420, total_slots: 15, available_bikes: 3, location_name: 'Ruoholahti'},
-    {lat: 60.1699, lng: 24.9384, total_slots: 25, available_bikes: 0, location_name: 'Niemenmäki'}
-  ];
+  render();
+}
 
+function getColor(value){
+  //value from 0 to 1
+  var hue=((1-value)*120).toString(10);
+  return ["hsl(",hue,",100%,50%)"].join("");
+}
+
+function render() {
   coords.forEach(function(coord) {
     var cityCircle = new google.maps.Circle({
       strokeColor: getColor(1),
@@ -36,10 +47,4 @@ function initMap() {
       infoWindow.open(map);
     });
   });
-}
-
-function getColor(value){
-  //value from 0 to 1
-  var hue=((1-value)*120).toString(10);
-  return ["hsl(",hue,",100%,50%)"].join("");
 }
