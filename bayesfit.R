acb360bcd376c485ecb524ce2b97aa1ba92ee94e
Henri Ikonen <henri.ikonen@sok.fi>
2016-08-30 20:03:59 +0300

    update

--- bayesfit.R
+++ bayesfit.R
@@ -1,50 +1,55 @@
 library(MASS)
 
-params=2
-samples=20
-be=matrix(c(2,3),2,1)
-x=matrix(rexp(200, rate=.1),samples,params)
-y=x%*%be+rnorm(20,0,1)
-wt=matrix(runif(params*1), params,1)
 
-numpar=1+params+samples
-allwt=matrix(runif(numpar*1), numpar, 1)
+bayesReg <- function(x, y, params = c(burn = 300,
+				      rounds = 500,
+				      thin = 10,
+				      nu = 3,
+				      allwt =,
+				      prior =,)) {
+					
 
-burn=300
-rounds=500
-thin=10
+params <- ncol(x)
+samples <- nrow(x)
+#be = matrix(c(2,3),2,1)
+#x=matrix(rexp(200, rate=.1),samples,params)
+#y=x%*%be+rnorm(20,0,1)
+
+numpar <- 1 + params + samples
+
+if (length(allwt) == 0) {
+	allwt <- matrix(runif(params*1), params,1)
+}
+
+wt <- allwt[2:(numpar+1)]
 
 for (n in 1:burn){
-allwt=bayesSample(allwt,wt,x,y)
+	allwt <- bayesSample(allwt,wt,x,y)
 }
 
-weights=matrix(0,params,rounds)
-hyper=matrix(0,1,rounds)
-for (i in 1:rounds){
-for (j in 1:thin){
-allwt = bayesSample(allwt,param,x,y)
-}
-  weights[,i]=allwt[2:(params+1)]
-  hyper[,i]=allwt[1]
-}
+weights <- matrix(0,params,rounds)
+hyper <- matrix(0,1,rounds)
 
-repmat = function(X,m,n)
-  {
-  ##R equivalent of repmat (matlab)
-  mx = dim(X)[1]
-  nx = dim(X)[2]
-  matrix(t(matrix(X,mx,nx*n)),mx*m,nx*n,byrow=T)
+for (i in 1:rounds) {
+	for (j in 1:thin){
+		allwt <- bayesSample(allwt,param,x,y)
+	}
+  	weights[,i] <- allwt[2:(params+1)]
+  	hyper[,i] <- allwt[1]
 }
 
+repmat <- function(X,m,n) {
+	mx <- dim(X)[1]
+  	nx <- dim(X)[2]
+  	return(matrix(t(matrix(X,mx,nx*n)),mx*m,nx*n,byrow=T))
+}
 
-bayesSample = function(allwt,wt,x,y)
-{
-  nu = 3
-  numpar=ncol(x)
-  numsamp=nrow(x)
-  beta=allwt[1]
-  wt=allwt[2:(numpar+1)]
-  w = allwt[(numpar+2):nrow(allwt)]
+bayesSample = function(allwt, wt, x, y, nu, prior) {
+  numpar <- ncol(x)
+  numsamp <- nrow(x)
+  beta <- allwt[1]
+  wt <- allwt[2:(numpar+1)]
+  w <-  allwt[(numpar+2):nrow(allwt)]
   
   
   resid=(y-x%*%wt)
