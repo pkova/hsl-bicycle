08a4622bdef6234b9db708a3b7aa94176ece009f
Pyry Kovanen <pyry.kovanen@gmail.com>
2016-08-30 21:00:18 -0700

    Update dummy data format, render function

--- client/main.js
+++ client/main.js
@@ -1,10 +1,9 @@
 var map;
-var circles = [];
 
 var data = [
-  {lat: 60.165092, lng: 24.930971, total_slots: 20, available_bikes: 1, location_name: 'Kaisaniemi', time: 2},
-  {lat: 60.158126, lng: 24.945420, total_slots: 15, available_bikes: 3, location_name: 'Ruoholahti', time: 1},
-  {lat: 60.1699, lng: 24.9384, total_slots: 25, available_bikes: 0, location_name: 'Niemenmäki', time: 0}
+  {lat: 60.165092, lng: 24.930971, location_name: 'Kaisaniemi', predictions: [{timestamp: 0, total_slots: 25, available_bikes: 5}, {timestamp: 1, total_slots: 25, available_bikes: 4}, {timestamp: 2, total_slots: 25, available_bikes: 2}]},
+  {lat: 60.158126, lng: 24.945420, location_name: 'Ruoholahti', predictions: [{timestamp: 0, total_slots: 25, available_bikes: 7}, {timestamp: 1, total_slots: 25, available_bikes: 7}, {timestamp: 2, total_slots: 25, available_bikes: 0}]},
+  {lat: 60.1699, lng: 24.9384,  location_name: 'Niemenmäki', predictions: [{timestamp: 0, total_slots: 25, available_bikes: 0}, {timestamp: 1, total_slots: 25, available_bikes: 1}, {timestamp: 2, total_slots: 25, available_bikes: 5}]}
 ];
 
 document.querySelector('.slider').addEventListener('input', function(e) {
@@ -25,7 +24,8 @@ function initMap() {
   createLocations();
 }
 
-function getColor(value){
+function getColor(bikes){
+  var value = Math.min(bikes / 6, 1);
   //value from 0 to 1
   var hue=((1-value)*120).toString(10);
   return ["hsl(",hue,",100%,50%)"].join("");
@@ -45,7 +45,7 @@ function createLocations(time) {
     });
 
     var infoWindow = new google.maps.InfoWindow({
-      content: obj.location_name,
+      content: obj.location_name + ' ' + obj.predictions[0].available_bikes,
       position: {lat: obj.lat, lng: obj.lng}
     });
 
@@ -53,9 +53,13 @@ function createLocations(time) {
       infoWindow.open(map);
     });
 
-    circles.push({circle: cityCircle, info: infoWindow});
+    obj.circle = cityCircle;
+
   });
 }
 
 function render(time) {
+  data.forEach(function(obj) {
+    obj.circle.setOptions({strokeColor: getColor(obj.predictions[time].available_bikes), fillColor: getColor(obj.predictions[time].available_bikes)});
+  });
 }
